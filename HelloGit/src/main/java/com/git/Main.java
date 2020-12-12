97d9ab2b9ae807bed7dbeebe6c1a3168aaa9a911
86159 <2606129896@qq.com>
2020-12-12 15:12:56 +0800

    dev 分支的操作

--- HelloGit/src/main/java/com/git/Main.java
+++ HelloGit/src/main/java/com/git/Main.java
@@ -7,5 +7,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Git!!!");
         System.out.println("update 1");
+        System.out.println("dev branch");
     }
 }
